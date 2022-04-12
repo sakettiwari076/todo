2fbe653cbb95e6db9a01aa0b6d2b462e94555cf2
Saket <Sakettiwari076@gmail.com>
2022-04-13 00:08:43 +0530

    adding bug fixed

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,31 @@ const index = () => {
     "Buy milk",
     "Buy rice",
     "Buy clothes"
-    
   ]);
+  // const hanldeAdd is function made to add items on clicking add buttons 
+  const handleAdd = () => {
+    if(todoItem) {
+    setItems([
+      todoItem ,
+      ...items,
+    
+    ])
+    settodoItem("");
+  }
+  };
 return (
   <>
  <div>
 <h1>Todo App </h1>
 <div>
   <input type = "text" value={todoItem } onChange={(e) => settodoItem (e.target.value)} />
-  <button type = "button">Add</button>
+  <button type = "button" onClick= {handleAdd} >Add</button>
 </div>
 <ul>
   {items.map((item) => (
     <li key = {item} > {item}</li>
   ))}
-<li> {todoItem}</li>
+{/* <li> {todoItem}</li>  */}
 </ul>
  </div>
   </>
