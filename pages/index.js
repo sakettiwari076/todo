58d911de6842bd8476aa74f6f5d81f14c0e47459
Saket <Sakettiwari076@gmail.com>
2022-04-12 23:28:48 +0530

    settodo > text value as settodo > li to display

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 const index = () => {
+  const [todoItem, settodoItem] = useState("");
   const [items, setItems] = useState([
     "Buy milk",
     "Buy rice",
@@ -11,14 +12,14 @@ return (
  <div>
 <h1>Todo App </h1>
 <div>
-  <input type = "text" />
+  <input type = "text" value={todoItem } onChange={(e) => settodoItem (e.target.value)} />
   <button type = "button">Add</button>
 </div>
 <ul>
   {items.map((item) => (
     <li key = {item} > {item}</li>
   ))}
-
+<li> {todoItem}</li>
 </ul>
  </div>
   </>
