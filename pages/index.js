ac52f8612f9f00bcc7151b0393bca6fe37b703fc
Saket <Sakettiwari076@gmail.com>
2022-04-13 17:46:44 +0530

    err resolved + css added + k

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,94 @@
-import { useState } from "react";
-import {v4 as uuidv4 } from 'uuid';
 import cx from "classnames";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const index = () => {
-  const [todoItem, settodoItem] = useState("");
-  const [items, setItems] = useState([
-  {
-    id : "1234",
-    message : " buy milk",
-    done : false,
-  },
-  ]);
-  // const hanldeAdd is function made to add items on clicking add buttons and it is under the if statement and
-  // will add items only if some texts is inside the input 
-  const handleAdd = () => {
-    if(todoItem) {
-    setItems([
-      {
-        id : uuidv4(),
-        message : todoItem ,
-        done : false ,
-      },
-      ...items,
-    
-    ]);
-    settodoItem("");
-  }
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  const [todoItem, setTodoItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleEnter = (event) => {
+    if (event.key === "Enter") {
+      handleAdd();
+    }
   };
-  const handleToggle = (id) => {
+
+  const handleAdd = () => {
+    if (todoItem) {
+      setItems([
+        {
+          id: uuidv4(),
+          message: todoItem,
+          done: false,
+        },
+        ...items,
+      ]);
+
+      setTodoItem("");
+    }
+  };
+
+  const handleDone = (id) => {
     const _items = items.map((item) => {
-      if (item.id == id) {
+      if (item.id === id) {
         return {
           ...item,
-          done: ! item.done,
-          // done is changing the statuss of the item 
+          done: !item.done,
         };
       }
+
       return item;
     });
-  };
-return (
-  <>
-  <head>
-  <link href="/dist/main.css" rel="stylesheet"/>
 
-  </head>
- <div>
-<h1 className=""> Todo App </h1>
-<div>
-  <input type = "text" value={todoItem } onChange={(e) => settodoItem (e.target.value)} />
-  <button type = "button" onClick= {handleAdd} >Add</button>
-</div>
-<ul>
-  {items
-  .filter(({ done }) => !done)
-  .map(({id , message , done}) => (
-    <li key = {id} onClick={() =>handleToggle(id)} className-={cx( "item" , {done})} > 
-    {message}
-    </li>
-  ))}
-   {items
-  .filter(({ done }) => !done)
-  .map(({id , message , done}) => (
-    <li key = {id} onClick={() =>handleToggle(id)} className-={cx( "item" , {done})} > 
-    {message}
-    </li>
-  ))}
-{/* <li> {todoItem}</li>  */}
-</ul>
- </div>
-  </>
-);
-} ;
-export default index ;
+    setItems(_items);
+  };
+
+  return (
+    <div className="w-3/4 mx-auto text-center">
+      <div className="pt-12">
+        <h1 className="text-4xl todohead">Todo App</h1>
+      </div>
+
+      <div className="pt-12">
+        <input
+          type="text"
+          value={todoItem}
+          className="w-full rounded py-2 px-4 text-gray-900"
+          onChange={(e) => setTodoItem(e.target.value)}
+          onKeyDown={handleEnter}
+        />
+      </div>
+
+      <ul className="pt-12">
+        {items
+          .filter(({ done }) => !done)
+          .map(({ id, message }) => (
+            <li
+              key={id}
+              className={cx(styles.item)}
+              onClick={() => handleDone(id)}
+            >
+              {message}
+            </li>
+          ))}
+
+        {items
+          .filter(({ done }) => done)
+          .map(({ id, message }) => (
+            <li
+              key={id}
+              className={cx(styles.item, styles.done)}
+              onClick={() => handleDone(id)}
+            >
+              {message}
+            </li>
+          ))}
+      </ul>
+      <button type="button">Instructions</button>
+
+    </div>
+  );
+};
+
+export default Home;
